e36bbd1bc6e291bfb58d07e8ba62d771bc178d22
Joe Tito <joetito1@gmail.om>
2024-09-12 20:38:57 -0400

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
+        <div className="list-inside list-decimal text-4xl text-center sm:text-left font-[family-name:var(--font-geist-mono)] text-green-400">
+          SourceCodeLabs
+        </div>
+        Insert witty tagline here
+        
+        {/* <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
@@ -47,9 +52,9 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -95,7 +100,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
